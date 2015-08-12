dbb3c90d4c0191b73035b8eecf62c8b44f67de68
jermspeaks <jermspeaks@gmail.com>
2015-08-12 00:32:58 -0700

    sample head

--- add_imdb_features.rb
+++ add_imdb_features.rb
@@ -76,10 +76,12 @@ def create_new_csv(file, movie_array)
   end
 end
 
-read_csv(db_file, movie_array)
-create_new_csv(new_db_file, movie_array)
+# read_csv(db_file, movie_array)
+# create_new_csv(new_db_file, movie_array)
 # puts movie_array
-
+imdb = FilmBuff::IMDb.new
+movie = imdb.find_by_id('tt1568346')
+print [movie.title, "http://www.imdb.com/title/tt1568346", "tt1568346", "01/2012", "#{movie.tagline}", "#{movie.plot}", movie.runtime, movie.rating, movie.votes, movie.poster_url, "#{movie.genres.join(", ")}", movie.release_date].join(",")
 
 
 
